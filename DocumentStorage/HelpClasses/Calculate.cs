1c25b0f648342cb9d9c82383c5e61984c132918b
Novator <dmitrij.novator@yandex.ru>
2024-10-09 23:50:26 +0500

    Added comments to the methods

--- DocumentStorage/HelpClasses/Calculate.cs
+++ DocumentStorage/HelpClasses/Calculate.cs
@@ -1,19 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DocumentStorage.HelpClasses
 {
     class Calculate
     {
+        /// <summary>
+        /// Фильтрация при условии строгого совпадения текста поиска
+        /// </summary>
+        /// <param name="txt">значение переменной документа</param>
+        /// <param name="searchTxt">искомый текст</param>
+        /// <param name="caseSensitive">должен ли учитываться регистр</param>
         public bool ContainsAllInStrictOrder(string txt, string searchTxt, bool caseSensitive)
         {
             txt = IsCaseSensitive(txt, caseSensitive);
             searchTxt = IsCaseSensitive(searchTxt, caseSensitive);
             return txt.Contains(searchTxt);
         }
+
+        /// <summary>
+        /// Фильтрация при условии совпадения всех слов поска в произвольном порядке
+        /// </summary>
+        /// <param name="txt">значение переменной документа</param>
+        /// <param name="searchTxt">искомый текст</param>
+        /// <param name="caseSensitive">должен ли учитываться регистр</param>
         public bool ContainsAllInAnyOrder(string txt, string searchTxt, bool caseSensitive)
         {
             txt = IsCaseSensitive(txt, caseSensitive);
@@ -28,6 +38,13 @@ namespace DocumentStorage.HelpClasses
             }
             return true;
         }
+
+        /// <summary>
+        /// Фильтрация при условии совпадения хотя бы одного слова из поиска
+        /// </summary>
+        /// <param name="txt">значение переменной документа</param>
+        /// <param name="searchTxt">искомый текст</param>
+        /// <param name="caseSensitive">должен ли учитываться регистр</param>
         public bool ContainsAtLeastOne(string txt, string searchTxt, bool caseSensitive)
         {
             txt = IsCaseSensitive(txt, caseSensitive);
@@ -42,6 +59,13 @@ namespace DocumentStorage.HelpClasses
             }
             return false;
         }
+
+        /// <summary>
+        /// Преобразование регистра текста согласно условию
+        /// </summary>
+        /// <param name="str">текст</param>
+        /// <param name="caseSensitive">должен ли учитываться регистр</param>
+        /// <returns>Если регист не учитывается, возвращает текст без изменений. Иначе переводит в нижний регистр</returns>
         private string IsCaseSensitive(string str, bool caseSensitive)
         {
             str = string.IsNullOrEmpty(str) ? string.Empty : str;
@@ -51,6 +75,10 @@ namespace DocumentStorage.HelpClasses
             }
             return str.ToLower();
         }
+
+        /// <summary>
+        /// Сравнение двух массивов байт
+        /// </summary>
         public bool IsEqualByteArrays(byte[] bArr1, byte[] bArr2)
         {
             if (bArr1 == null && bArr2 == null)
